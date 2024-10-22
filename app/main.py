76dda70eacebd6f204e2a2761b3f3ceb221b0289
alecesar27 <147946872+alecesar27@users.noreply.github.com>
2024-10-21 21:03:27 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -91,6 +91,3 @@ async def total_hectares():
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
-
-    if __name__ == "__main__":
-        uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
